e50b9705fe7f14e83146a30a143fdd856e9eb154
Raydev <rowlandoka@gmail.com>
2023-01-10 02:34:51 +0100

    Added Inheritances

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+ Check if object is instance of a class, or instance of inherited class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Return"""
+    return isinstance(obj, a_class)
