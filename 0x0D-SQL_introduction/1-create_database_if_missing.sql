b86180d321d21d199f8ec642afe0a25c4220ae46
Rowland Oka <rowlandoka@gmail.com>
2023-02-15 06:52:40 +0100

    creating and deleting database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- create a database name hbtn_0c_0 in your mysql server
+--If the database hbtn_0c_0 already exists, your script should not fail
+CREATE DATABASES IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes a database name hbtn_0c_0 in your MYSQL SERVER
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
