5f9eb383d3a66cecfb212e08d15ffd6720a8cb15
Rowland Oka <rowlandoka@gmail.com>
2023-02-17 00:00:46 +0100

    print full table description

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+--lists all the record of the second_table in mysql server
+SELECT *
+FROM `second_table`
+ORDER BY `score` ASC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+--list all records with score greater than 10
+SELECT *
+FROM `second_table`
+WHERE `score` >= 10;
