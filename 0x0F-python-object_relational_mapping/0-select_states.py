af0d6376054807d610f3369361891c4822767341
Rowland Oka <rowlandoka@gmail.com>
2023-03-18 14:13:12 +0100

    Python object relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-# Get all states via python
+# gets all states via python
 
 
 def main(args):
-    # Get states
+    # gets all state
     if len(args) != 4:
-        raise Exception('Needs 3 arguments')
-    db = MySQLdb.connect(
-        host='localhost', user=args[1], passwd=args[2], db=args[3])
+        raise Exception("need 3 arguments!")
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states  ORDER BY id ASC')
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
