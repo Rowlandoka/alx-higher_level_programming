1446c8f452d9ef304e9824f647973f5a60e7a605
Rowland Oka <rowlandoka@gmail.com>
2023-04-01 01:00:19 +0100

    make response and post request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 # fetches a website
-import urllib.request
+from urllib.request import urlopen
 
 
 def main():
     """
     fetches url using urllib package
     """
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+    with urlopen('https://alx-intranet.hbtn.io/status') as resp:
         content = resp.read()
-        url = content.decode('utf-8')
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(url))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
 
 
 if __name__ == "__main__":
